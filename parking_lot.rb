ae0c6dbc47284eca792ce1e86981463d07e2b928
yujiorama <yujiorama+github@gmail.com>
2014-05-08 09:30:17 +0900

    generate parking lots from list

--- parking_lot.rb
+++ parking_lot.rb
@@ -8,17 +8,43 @@ Dir['vendor/*'].each do |lib|
   $:.unshift(File.join(File.dirname(__FILE__), lib, 'lib'))
 end
 
+class ParkcalcForm
+  attr :parkingLot
+  attr :startingDate
+  attr :startingTime
+  attr :startingTimeAMPM
+  attr :leavingDate
+  attr :leavingTime
+  attr :leavingTimeAMPM
+
+  def from(params)
+    @parkingLot = params[:parkingLot]
+    @startingDate = params[:startingDate]
+    @startingTime = paras[:startingTime]
+    @startingTimeAMPM = params[:startingTimeAMPM]
+    @leavingDate = params[:leavingDate]
+    @leavingTime = params[:leavingTime]
+    @leavingTimeAMPM = params[:leavingTimeAMPM]
+  end
+end
+
 class ParkingLotApp < Sinatra::Base
   helpers do
     include Rack::Utils; alias_method :h, :escape_html
   end
+  
+  def parkingLots
+    {"1" => "Valet Parking", "9" => "Takashimaya"}
+  end
 
   get '/parkcalc' do
+    @parkingLots = parkingLots
     @estimatedParkingCosts = "$ 0.00"
     erb :parkcalc
   end
   
   post '/parkcalc' do
+    form = ParkcalcForm.from(params)
     @estimatedParkingCosts = "$ 12.00"
     erb :parkcalc
   end
